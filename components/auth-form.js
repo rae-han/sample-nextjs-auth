0386060b373ff2b1b3b9ceaea3b230590286cdf2
raehan <raehan900809@gmail.com>
2024-06-25 09:44:55 +0900

    feat: create user data

--- components/auth-form.js
+++ components/auth-form.js
@@ -1,8 +1,17 @@
+'use client';
+
 import Link from 'next/link';
+// import { useActionState } from "react";
+import { useFormState } from 'react-dom';
+
+import { signup } from '@/actions/auth-actions';
 
 export default function AuthForm() {
+  // const [formState, formAction] = useActionState(signup, {})
+  const [formState, formAction] = useFormState(signup, {})
+
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
